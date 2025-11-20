d94032c7f45128e09dfeded883b2d1a9013d40d0
Hannah Christine Rapal <hanakristinhc@gmail.com>
2025-11-20 19:54:07 +0800

    NOV. 20 - 7:53

--- CLASSES/Track.py
+++ CLASSES/Track.py
@@ -3,33 +3,11 @@
 
 class Track:
     def __init__(self, title, artist, feat, album, duration):
-        self.__title = title
-        self.__artist = artist
-        self.__feat = feat
-        self.__album = album
-        self.__duration = duration
-
-    def getTitle(self):
-        return self.__title
-    def getArtist(self):
-        return self.__artist
-    def getFeat(self):
-        return self.Feat
-    def getAlbum(self):
-        return self.__album
-    def getDuration(self):
-        return self.__duration
-    
-    def setTitle(self, newTitle):
-        self.__title = newTitle
-    def setArtist(self, newArtist):
-        self.__artist = newArtist
-    def setFeat(self, newFeat):
-        self.__feat = newFeat
-    def setAlbum(self, newAlbum):
-        self.__album = newAlbum
-    def setDuration(self, newDuration):
-        self.__duration = newDuration
+        self.title = title
+        self.artist = artist
+        self.feat = feat
+        self.album = album
+        self.duration = duration
 
     def store_dict(self):
         return {
