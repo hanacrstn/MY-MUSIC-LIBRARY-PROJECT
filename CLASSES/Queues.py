8b679b76e1464b5a44d1f5c777de5b7a0e1c00aa
bell189 <bellnaqz06@gmail.com>
2025-12-02 08:47:02 +0800

    Update Queues.py

--- CLASSES/Queues.py
+++ CLASSES/Queues.py
@@ -1,9 +1,26 @@
 
 #QUEUES CLASS
 
+class Node:
+    def __init__(self, track=None):
+        self.track = track
+        self.next = None
+
+
 class Queue:
     def __init__(self):
-        pass
+        self.t_first = None
+        self.t_last = None
+        self.t_current = None
+        self.n_tracks = 0
+
+       
+        self.shuffle_ = False
+        self.repeat_ = False
+        self.playing_ = False
+
+        
+        self.queuelist = []
 
     def save_queue(self):
         pass
@@ -24,16 +41,66 @@ class Queue:
         pass
 
     def next_track(self):
-        pass
+        
+        if self.n_tracks == 0:
+            return "Queue is empty. No tracks to play."
+
+        if self.shuffle_:
+            return self.shuffle()
+
+        if not self.current:
+            self.current = self.t_first
+            
+        else:
+            if self.current == self.t_last and not self.repeat_:
+                return self.rear.track
+            self.current = self.current.next if self.current != self.t_last else self.t_first
+
+        return self.current.track
 
     def previous_track(self):
-        pass
+        if self.n_tracks == 0:
+            return "Queue is empty. No tracks to play."
+
+        if not self.current:
+            self.current = self.front
+            return self.current.track
+
+        if self.current == self.front:
+            if self.repeat_:
+                self.current = self.rear
+                return self.current.track
+            return "No previous track. You're at the first track."
+
+        temp = self.front
+        while temp.next != self.current:
+            temp = temp.next
+
+        self.current = temp
+        return self.current.track
 
     def play(self):
-        pass
+        
+        if not self.current:
+            return "No track is currently playing."
+
+        t = self.current.track
+        return f"{t['title']} by {t['artist']} ({t['duration']})"
 
     def add_track(self, track):
-       if track is None:
+         new_node = Node(track)
+
+        if self.n_tracks == 0:
+            self.t_first = self.rear = new_node
+            new_node.next = new_node
+        else:
+            self.t_last.next = new_node
+            self.t_last = new_node
+            self.t_last.next = self.t_first
+
+        self.n_tracks += 1
+        self.queuelist.append(new_node)
+        return True
             
             
     def add_play(self, playlist):
