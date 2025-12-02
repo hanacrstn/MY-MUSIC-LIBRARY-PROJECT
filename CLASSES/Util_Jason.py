531198e744bb1c496faaeedd2e679813c14b5678
Laurence-12 <182446037+Laurence-12@users.noreply.github.com>
2025-12-02 14:04:56 +0800

    Util_Jason

--- CLASSES/Util_Jason.py
+++ CLASSES/Util_Jason.py
@@ -2,10 +2,10 @@ import json
 
 def load():
     try:
-        with open("Store.json", 'r') as file:
+        with open("Storage.json", 'r') as file:
             return json.load(file)
     except FileNotFoundError:
-        print(f"File {"Store.json"} not found. Returning an empty list.")
+        print(f"File {"Storage.json"} not found. Returning an empty list.")
         return []
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
@@ -13,7 +13,7 @@ def load():
 
 def save(data):
     try:
-        with open("Store.json", 'w') as file:
+        with open("Storage.json", 'w') as file:
             json.dump(data, file, indent=4)
     except Exception as e:
         print(f"Error saving JSON: {e}")
